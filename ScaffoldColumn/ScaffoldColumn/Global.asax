0ff0f42cd78e4d1335beb6dc1b1ed575e01c5ee8
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-05-09 21:14:58 +0530

    Scaffold Column

---
+++ ScaffoldColumn/ScaffoldColumn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScaffoldColumn.MvcApplication" Language="C#" %>
---
+++ ScaffoldColumn/ScaffoldColumn/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScaffoldColumn.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+       
+        [ScaffoldColumn(false)]
+        public string Contact { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Address { get; set; }
+    }
+}
